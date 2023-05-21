ca1a837648402facd9a649c03744cd308d42108a
isaacpradford <radf.isaac@gmail.com>
2023-05-20 20:19:54 -0600

    Fixed incorrect host link.

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Contacts API'
     }, 
-    host: 'https://cse341-5zah.onrender.com',
+    host: 'cse341-5zah.onrender.com',
     // host: 'localhost:8801',
     schemes: ['https', 'http']
 };
